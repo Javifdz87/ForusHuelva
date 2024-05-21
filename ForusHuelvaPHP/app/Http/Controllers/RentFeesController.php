6f70b6d60bc0cf9f221a7b10bb4639e66c82a44f
Javifdz87 <javifdz8703@gmail.com>
2024-05-21 18:20:51 +0200

    update

--- ForusHuelvaPHP/app/Http/Controllers/RentFeesController.php
+++ ForusHuelvaPHP/app/Http/Controllers/RentFeesController.php
@@ -2,28 +2,28 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\rentalFeesModel;
+use App\Models\RentalFeesModel;
 use Illuminate\Http\Request;
 
 class RentFeesController extends Controller
 {
     public function index()
     {
-        $rentals = rentalFeesModel::with('client:id,name,email', 'sport:sport')->get();;
+        $rentals = RentalFeesModel::with('client:id,name,email', 'sport:sport')->get();;
 
         return response()->json($rentals);
     }
 
     public function store(Request $request)
 {
-    $rent = rentalFeesModel::create($request->all());
+    $rent = RentalFeesModel::create($request->all());
 
     return response()->json($rent, 201);
 }
 
     public function update(Request $request, $id)
     {
-        $rent = rentalFeesModel::findOrFail($id);
+        $rent = RentalFeesModel::findOrFail($id);
         $rent->update($request->all());
 
         return response()->json($rent, 200);
@@ -31,7 +31,7 @@ class RentFeesController extends Controller
 
     public function destroy($id)
     {
-        rentalFeesModel::destroy($id);
+        RentalFeesModel::destroy($id);
 
         return response()->json(null, 204);
     }
